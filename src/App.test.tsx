3005a9bde8a8b9f1f7a1e2705abf1489de497a45
Vendula Kudejova <vendula.kudejova@gmail.com>
2022-12-16 23:21:07 +0100

    adds unit test for app.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+describe('App rendering tests', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+  test('renders heading', () => {
+    const heading: HTMLHeadingElement = screen.getByText(/ToDo App/i);
+    expect(heading).toBeInTheDocument();
+  });
+})
+
